ebb8e95f55c092afd614a75052ec04dd22f4f7a9
naekomiki <bu2277350008@bukkyo-u.ac.jp>
2024-07-19 15:39:13 +0900

    Initial commit

---
+++ s1/main.rb
@@ -0,0 +1,3 @@
+puts 'Hello, World!'
+puts'my name is Naeko Miki'
+pups'putsnoyarikatadaeatteimasuka'
---
+++ s2/main.rb
@@ -0,0 +1,5 @@
+puts 'Hello, World!'
+puts'konnichiha'
+puts'Hello,World'.downcase
+
+puts 2.0/3.0
