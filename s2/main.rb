a34a86688b3c909cca0e668dafd13ebd2ec0fbce
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-20 10:07:09 +0900

    Update main.rb

--- s2/main.rb
+++ s2/main.rb
@@ -1,5 +1,23 @@
 puts 'Hello, World!'
+
 puts'konnichiha'
-puts'Hello,World'.downcase
+
+puts 
+
+puts 1000
+
+puts 3.1415
+
+puts 1+123
+
+puts 1-123
+
+puts 2*3
+
+puts 2/3
 
 puts 2.0/3.0
+
+puts'Hello,World'.downcase
+
+put 1.to_f
