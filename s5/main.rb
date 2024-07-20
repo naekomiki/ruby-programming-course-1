07ae745cd8100b26e11bab47c03e21430d248c69
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-20 11:27:41 +0900

    Update main.rb

--- s5/main.rb
+++ s5/main.rb
@@ -1,1 +1,20 @@
-puts 'Hello, World!'
+def triple(number)
+    number * 3
+end
+puts triple(100)
+
+require_relative 'plant'
+ 
+ plant_aloe = Plant.new
+puts plant_aloe.height
+
+now = Time.now
+puts now
+
+current_time = Time.now
+current_time = Time.now <-現在の時間を取得して、新しいインスタンスを作る
+puts current_time.class
+
+require 'date'
+current_date = Date.today
+puts current_date
