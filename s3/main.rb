581c2ca3da9395beec2b15410504cdd150e73f07
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-20 10:15:32 +0900

    Update main.rb

--- s3/main.rb
+++ s3/main.rb
@@ -1,1 +1,79 @@
 puts 'Hello, World!'
+one=1
+
+three_point_zero=3.0
+
+puts three_point_zero
+
+puts one/three_point_zero
+
+1plus1
+
+two=1+1
+
+_012='Hello,World'
+
+puts _012
+
+h_w='Hello,World'
+
+puts h_w
+
+abcdefghijklmnopqrstuvwz='some spellings'
+
+puts abcdefghijklmnopqrstuvwz
+
+wallet=2,000
+
+puts wallet
+
+wallet='Enpty'
+
+puts wallet
+
+puts 2,000==2000==2,0,0,0
+
+wallet=1_000
+
+drink=100
+
+wallet-now=wallet-drink
+
+def twice(number)
+    number*2
+end
+
+puts twice(100)
+
+def twice(number1,number2)
+    (number1+number2)*2
+end
+
+puts twice(100,200)
+
+def twice(number)
+    number*2
+end
+
+def hundled
+    100
+end
+
+puts hundled
+
+def メソッド名（引数）
+    処理
+end
+
+def hello(something)
+    puts "Say hello to"+something
+end
+
+def twice_plus_one(number)
+    twice=number*2
+    twice+1
+end
+
+var=twice_plus_one(100)
+
+puts var
